5a321c3f00ed7818eb2602753adf62738f342e62
GimaYuto <yurihanagima@gmail.com>
2022-10-08 14:30:21 +0900

    スキーマの追加

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const users = [
+    {id: "1", name: "Amanda", email: "amanda@example.com"},
+    {id: "2", name: "Billy", email: "billy@example.com"},
+]
+
 const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
+
     type Query{
         hello(name: String!): String
+        users: [User]
+        user(id: ID!): User
     }
 `
 
 const resolvers = {
     Query: {
         hello: (parent, args) =>{
-            console.log("parent", parent)
-            console.log("atgs", args)
             return `Hello ${args.name}`;
+        },
+        users: () => users,
+        user: (parent, args) => {
+            const user = users.find((user) => user.id === args.id);
+            return user;
         },
     },
 }
